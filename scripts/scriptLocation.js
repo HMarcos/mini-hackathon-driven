70dcbb8f14a6cba09f50fb734818127216a1860c
Icaro-pavani <icaro.pavani79@gmail.com>
2022-03-04 20:03:25 -0300

    layout inicio

--- scripts/scriptLocation.js
+++ scripts/scriptLocation.js
@@ -2,6 +2,11 @@ const GEOCODING_LINK = "http://api.openweathermap.org/geo/1.0";
 const API_KEY = "8bdb71c1758a1a69b7f51aa4dc56fe95";
 
 const botaoBusca = document.querySelector("button");
+const titulo = document.querySelector(".titulo");
+
+titulo.addEventListener("click", () => {
+    window.location.reload();
+})
 
 let longitude;
 let latitude;
